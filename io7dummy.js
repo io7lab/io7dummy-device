b8df87b30c356e244ca1377d1489d8a7488b1c42
Yoonseok Hur <yoonseok@gmail.com>
2024-05-28 10:36:59 +0900

    command parameter update

--- io7dummy.js
+++ io7dummy.js
@@ -3,10 +3,15 @@ import {setConfigFile} from './io7device.js';
 import {runLamp } from './lamp.js';
 import {runSwitch } from './switch.js';
 
-if (process.argv.length < 3 || process.argv[2] === 'lamp') {
+if (process.argv.length < 3) {
+    console.log('Usage: io7dummy.js  lamp | switch');
+    process.exit(1);
+}
+
+if (process.argv[2] === 'lamp') {
     setConfigFile('lamp.cfg');
     runLamp();
-} else {
+} else if (process.argv[2] === 'switch') {
     setConfigFile('switch.cfg');
     runSwitch();
 }
