3f202ef550d37b1c18dcf4c3ff9ef04b972a19a7
Yoonseok Hur <yoonseok@gmail.com>
2025-10-30 17:37:04 +0900

    code refined

--- valve.js
+++ valve.js
@@ -5,31 +5,31 @@ const cursorUp = '\x1B[A'; // Move cursor up one line
 let valve = 'off';
 
 function offValve() {
-  clearCursor();
-  console.log("                               ");  
-  console.log("                               ");  
-  console.log("          _____-_____          ");  
-  console.log("               |               ");  
-  console.log("          _____|_____          ");  
-  console.log("                               ");  
-  console.log("          Valve Closed         ");  
-  console.log("                               ");  
-  console.log("                               ");  
-  valve = 'off';
+    clearCursor();
+    console.log("                               ");  
+    console.log("                               ");  
+    console.log("          _____-_____          ");  
+    console.log("               |               ");  
+    console.log("          _____|_____          ");  
+    console.log("                               ");  
+    console.log("          Valve Closed         ");  
+    console.log("                               ");  
+    console.log("                               ");  
+    valve = 'off';
 }
 
 function onValve() {
-  clearCursor();
-  console.log("                               ");  
-  console.log("               T               ");  
-  console.log("          _____|_____          ");  
-  console.log("                               ");  
-  console.log("          ___________          ");  
-  console.log("                               ");  
-  console.log("           Valve Open          ");  
-  console.log("                               ");  
-  console.log("                               ");  
-  valve = 'on';
+    clearCursor();
+    console.log("                               ");  
+    console.log("               T               ");  
+    console.log("          _____|_____          ");  
+    console.log("                               ");  
+    console.log("          ___________          ");  
+    console.log("                               ");  
+    console.log("           Valve Open          ");  
+    console.log("                               ");  
+    console.log("                               ");  
+    valve = 'on';
 }
 
 export function init(device) {
@@ -37,32 +37,33 @@ export function init(device) {
     stdin.setRawMode(true);
     stdin.resume();
     stdin.setEncoding('utf8');
-    stdin.on('data', function(key){
+    stdin.on('data', function(key) {
         if (key === '\u0003' || key === '\u001b') {
-            process.exit(); 
+          process.exit();
         }
     });
-  device.setUserCommand((topic, msg) => {
-    console.log('command', JSON.parse(msg));
-    let cmd = JSON.parse(msg);
-    if (cmd.hasOwnProperty('d') && cmd.d.hasOwnProperty('valve')) {
-      if (cmd.d.valve === 'on') {
-        onValve();
-      } else {
-        offValve();
-      }
-      device.publishEvent('status', JSON.stringify({"d":{"valve":valve}}));
-      console.log(`\x1B[0m      {"d":{"valve":"${valve}"}`, cursorUp);
-    }
-  });
 
-  device.loop = () => {
-    device.publishEvent('status', JSON.stringify({"d":{"valve":valve}}));
-    console.log(`\x1B[0m      {"d":{"valve":"${valve}"}`, cursorUp);
-  };
+    device.setUserCommand((topic, msg) => {
+        console.log('command', JSON.parse(msg));
+        let cmd = JSON.parse(msg);
+        if (cmd.hasOwnProperty('d') && cmd.d.hasOwnProperty('valve')) {
+            if (cmd.d.valve === 'on') {
+                onValve();
+            } else {
+                offValve();
+            }
+            device.publishEvent('status', JSON.stringify({ "d": { "valve": valve } }));
+            console.log(`\x1B[0m      {"d":{"valve":"${valve}"}`, cursorUp);
+        }
+    });
 
-  offValve();
+    device.loop = () => {
+        device.publishEvent('status', JSON.stringify({ "d": { "valve": valve } }));
+        console.log(`\x1B[0m      {"d":{"valve":"${valve}"}`, cursorUp);
+    };
 
-  device.connect();
-  device.run();
+    offValve();
+
+    device.connect();
+    device.run();
 }
